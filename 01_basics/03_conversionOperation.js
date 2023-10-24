f53b7adde143934b5b397bcdf6572d22396cc9f0
Sunny Patel <sunnypatel.koder@gmali.com>
2023-10-24 14:33:02 +0530

    diffrent type of operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
-let score = 1
+// let score = 1
 // console.log(typeof score);
 // console.log(typeof(score));
-let valueInNumber = Number(score);
+// let valueInNumber = Number(score);
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber)
 
@@ -9,12 +9,52 @@ let valueInNumber = Number(score);
 // "33abc" => NaN
 // true => 1: false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = Number(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Number(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber,stringNumber)
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber,stringNumber)
+
+// *******************Operations****************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// why?
+// console.log(+true)
+// console.log(+ "")
+
+let num1 , num2, num3 ;
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+
+// link
+// https://tc39.es/ecma262/#sec-toprimitive

593c7ad68988f784cc0a348e5d80791bf9fd3aad
Sunny Patel <sunnypatel.koder@gmali.com>
2023-10-24 14:08:31 +0530

    dataTypes-ConversiongOperation

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // we are using node js , not broweser
+
+console.log(3+3)
+
+// code readibility should be high
+
+let name = "sunny"
+let age = "24"
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined)
